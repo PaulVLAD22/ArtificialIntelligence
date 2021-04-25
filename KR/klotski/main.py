a00e033ccf20e0c1d54bf06c4ab6702aa2a01635
paulvlad22 <paulvlad34@gmail.com>
2021-04-25 10:56:25 +0300

    ,

---
+++ KR/klotski/main.py
@@ -0,0 +1,4 @@
+# Klotski - Ai piese de mutat in matrice
+# mutam toate elementele cu A sau cu B sau C mai la stanga / mai la dreapta / in ce directii trtebuie
+# le facme inainte de vacanta ca sa am timp dupa
+
